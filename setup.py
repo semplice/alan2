5560921d9135d20597ba54a5c87ceedcda35fdb8
Eugenio Paolantonio (g7) <me@medesimo.eu>
2013-12-20 15:57:08 +0100

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# alan2 - An openbox menu builder
+# Copyright (C) 2013  Semplice Project
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Authors:
+#    Eugenio "g7" Paolantonio <me@medesimo.eu>
+#
+
+from distutils.core import setup
+
+setup(
+	name='alan2',
+	version='0.0.1',
+	description='Openbox Menu Extension Framework and Builder',
+	author='Eugenio Paolantonio',
+	author_email='me@medesimo.eu',
+	url='https://github.com/semplice/alan2',
+	scripts=['alan-config.py', 'alan-menu-updater.py', 'alan-pipe.py', 'reference-watcher.py'],
+	packages=['alan', 'alan.core', 'alan.core.objects', 'alan.extensions'],
+	data_files=[
+		("/etc/alan", ["alan.conf"]),
+		(
+			"/etc/alan/watchers",
+			[
+				"watchers/xdgmenu.watcher",
+				"watchers/places.watcher",
+				"watchers/main.watcher",
+			]
+		)
+	
+	],
+	requires=['ConfigParser', 'commands', 'gettext', 'gmenu', 'locale', 'os', 'sys', 're', 'shutil', 'xml.etree.ElementTree', 't9n.library', 'gtk', 'nala'],
+)
